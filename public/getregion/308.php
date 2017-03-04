1c2cfb301d23434405591647fbf2dc0ea85a038b
liyutao <bianjili888@163.com>
2017-03-04 09:57:12 +0800

    fdasd

---
+++ public/getregion/308.php
@@ -0,0 +1,12 @@
+<?php
+return [ 
+'3086' => '{"region_id":3087,"parent_id":308,"region_name":"\u4e34\u7fd4\u533a","region_type":3}',
+'3087' => '{"region_id":3088,"parent_id":308,"region_name":"\u51e4\u5e86\u53bf","region_type":3}',
+'3088' => '{"region_id":3089,"parent_id":308,"region_name":"\u4e91\u53bf","region_type":3}',
+'3089' => '{"region_id":3090,"parent_id":308,"region_name":"\u6c38\u5fb7\u53bf","region_type":3}',
+'3090' => '{"region_id":3091,"parent_id":308,"region_name":"\u9547\u5eb7\u53bf","region_type":3}',
+'3091' => '{"region_id":3092,"parent_id":308,"region_name":"\u53cc\u6c5f\u62c9\u795c\u65cf\u4f64\u65cf\u5e03\u6717\u65cf\u50a3\u65cf\u81ea\u6cbb\u53bf","region_type":3}',
+'3092' => '{"region_id":3093,"parent_id":308,"region_name":"\u803f\u9a6c\u50a3\u65cf\u4f64\u65cf\u81ea\u6cbb\u53bf","region_type":3}',
+'3093' => '{"region_id":3094,"parent_id":308,"region_name":"\u6ca7\u6e90\u4f64\u65cf\u81ea\u6cbb\u53bf","region_type":3}',
+'3094' => '{"region_id":3095,"parent_id":308,"region_name":"\u5176\u5b83\u533a","region_type":3}',
+] ; 
