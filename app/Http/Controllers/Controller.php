c04c16fdd931ec9c23cde09c5bdbfbc735d9287d
yuepeng <995645653@qq.com>
2017-03-08 19:18:25 +0800

    zhaopin  buchong

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,10 +8,13 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+// use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+use App\Model\Admin\WebConfig\Region;
+use App\Model\Admin\WebConfig\Nav;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    
 
     public function __construct()
     {
